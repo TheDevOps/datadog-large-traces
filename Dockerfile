e323db83d65751902b862aed30095a3e861064cd
Daniel Hofer <daniel.hofer@porscheinformatik.com>
2025-06-10 11:53:46 +0200

    Enable ddprof
    - just fails to init

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM openjdk:21-jdk
 COPY entrypoint.sh /opt/entrypoint.sh
 COPY target/datadog-large-trace-0.0.1-SNAPSHOT.jar /opt/app.jar
-RUN chmod 555 /opt/entrypoint.sh
+COPY ddprof /opt/ddprof
+RUN chmod 777 /opt/entrypoint.sh /opt/ddprof
 ENTRYPOINT ["/opt/entrypoint.sh"]
 CMD ["startapp"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 #
 # docker-entrypoint for docker application
 
-export JAVA_OPTIONS="${JAVA_OPTIONS} ${JAVA_OPTS} -Dfile.encoding=UTF-8 -Xms256M -Xmx256M -Duser.timezone=UTC ${APP_OPTS}"
+export JAVA_TOOL_OPTIONS="-XX:NativeMemoryTracking=summary ${JAVA_OPTIONS} ${JAVA_OPTS} -Dfile.encoding=UTF-8 -Xms256M -Xmx256M -Duser.timezone=UTC ${APP_OPTS}"
 
 if [ "$1" = "startapp" ]; then
-    echo exec java -jar /opt/app.jar
+    echo exec /opt/ddprof java -jar /opt/app.jar
     echo "---------------------------------------------"
-    exec java -jar /opt/app.jar
+    exec /opt/ddprof java -jar /opt/app.jar
 else
     echo exec "$@"
     echo "---------------------------------------------"
